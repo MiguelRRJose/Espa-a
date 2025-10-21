b4f4704a32734c3e3941d5ee57df03076e4b8da2
MiguelRRJose <00095322@uca.edu.sv>
2025-10-20 19:32:23 -0600

    :tada:Arriba espania

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('enter-btn').addEventListener('click', () => {
+  alert('¡Olé!');
+  document.getElementById('welcome-screen').classList.add('hidden');
+  document.getElementById('content').classList.remove('hidden');
+});
